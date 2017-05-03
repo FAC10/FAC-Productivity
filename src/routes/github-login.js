5c59781b9ee2dcfe33ba1b1c6e65ed1db9f22802
finnhodgkin <finnhodgkin@gmail.com>
2017-05-03 14:39:37 +0100

    add oauth login route and eslintrc relates to #26

---
+++ src/routes/github-login.js
@@ -0,0 +1,19 @@
+const qs = require('querystring');
+
+module.exports = {
+  method: 'GET',
+  path: '/login-with-github',
+  config: {
+    auth: false,
+  },
+  handler: (req, reply) => {
+    const queryParams = {
+      client_id: process.env.CLIENT_ID,
+    };
+
+    const query = qs.stringify(queryParams);
+    const url = 'https://github.com/login/oauth/authorize?';
+
+    reply.redirect(url + query);
+  },
+};
