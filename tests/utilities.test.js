07fcc3bac5ecab77147df3aae794e8fc316f394d
Yvonne Liu <liuyvonne.ca@gmail.com>
2017-05-08 17:48:42 +0100

    finish utiities testing, add codecov badge to README
    #70 #16 #28

--- tests/utilities.test.js
+++ tests/utilities.test.js
@@ -1,3 +1,4 @@
+
 const test = require('tape');
 const { partial, parallel } = require('../src/lib/utilities');
 
@@ -11,4 +12,16 @@ test('Do partials work', (t) => {
   const add2 = partial(add3, 1, 1);
   t.equal(add2(1), 3, 'Should get 3');
   t.end();
+});
+
+test('Do parallel work', (t) => {
+  t.plan(2);
+  const fake = cb => cb(null, 1);
+  const fakeError = cb => cb('error');
+  parallel([fake, fake, fake], (error, result) => {
+    t.deepEqual(result, [[1], [1], [1]], 'Should return an array of arrays');
+  });
+  parallel([fakeError], (error) => {
+    t.deepEqual(error, 'error', 'error');
+  });
 });
