6dec7d1d16e877c24024198a9a9bb32e85545c4a
finnhodgkin <finnhodgkin@gmail.com>
2017-05-12 13:38:45 +0100

    .

--- src/socket/index.js
+++ src/socket/index.js
@@ -80,6 +80,8 @@ module.exports = (listener, childProcess) => {
         console.log('happening');
         child = exec('../rpi-rgb-led-matrix/utils/led-image-viewer ../rpi-rgb-led-matrix/utils/STAR.gif --led-rows=32 --led-chain=2');
         setTimeout(() => {
+          child.end();
+          child.kill();
           killProcess();
         }, 2000);
       }
