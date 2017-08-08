2c1e9eb97e21d93a1e1c62a87c770ee9a7e68b20
finnhodgkin <finnhodgkin@gmail.com>
2017-08-08 21:06:30 +0300

    re-deploy with database

--- src/database/get.js
+++ src/database/get.js
@@ -1,29 +1,28 @@
-connect = require('./connect')
+const connect = require('./connect');
 
-const get = {}
+const get = {};
 
 get.getNames = (callback) => {
-  connect.query(`SELECT name, selected, id FROM lpop;`, (err, { rows: names }) => {
-
+  connect.query('SELECT name, selected, id FROM lpop;', (err, { rows: names }) => {
     err ? callback(err) :
     // Error handle no results
     !names.length ? callback(new Error('No results')) :
     // Give callback an array of name strings
-    callback(null, {names: names.map( n => ({name: n.name, selected: n.selected, id: n.id}) ) })
-  })
-}
+    callback(null, { names: names.map(n => ({ name: n.name, selected: n.selected, id: n.id })) });
+  });
+};
 
 get.getCurrent = (callback) => {
-  connect.query(`SELECT name FROM current;`, (err, res) => {
+  connect.query('SELECT name FROM current;', (err, res) => {
     // Get current name
-    err ? callback(err) : callback(null, {currentName: res.rows[0].name})
-  })
-}
+    err ? callback(err) : callback(null, { currentName: res.rows[0].name });
+  });
+};
 
 get.allPop = (callback) => {
   connect.query('SELECT allp FROM allpop;', (err, res) => {
-    err ? callback(err) : callback(null, {allUsers: res.rows[0].allp})
-  })
-}
+    err ? callback(err) : callback(null, { allUsers: res.rows[0].allp });
+  });
+};
 
-module.exports = get
+module.exports = get;
